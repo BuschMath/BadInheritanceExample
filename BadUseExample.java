aee006b2711e5cd76a6731eddacde6663b0fd774
BuschMath <busch@iowacentral.edu>
2023-04-05 13:31:06 -0500

    Added commments

--- BadUseExample.java
+++ BadUseExample.java
@@ -1,3 +1,19 @@
+/*In this example, we have a superclass Animal that has two 
+    methods eat() and sleep(). The subclasses Dog, Cat, and 
+    Bird inherit from the Animal class, but they do not override 
+    or add any new functionality to the eat() and sleep() 
+    methods. However, the Dog, Cat, and Bird classes have 
+    additional methods bark(), meow(), and fly(), respectively.
+
+    In the BadUseExample class, we create instances of each 
+    subclass and call the eat() method on them. However, this 
+    demonstrates a bad use of inheritance because the Dog, Cat, 
+    and Bird classes do not actually need to inherit the eat() 
+    and sleep() methods from the Animal class since they do not 
+    add any new functionality to these methods. This can lead to 
+    unnecessary code bloat and confusion for future developers 
+    who may think that these methods are important to the subclasses. */
+
 public class BadUseExample{
     public static void main(String[] args) {
         Dog myDog = new Dog("Rufus");
